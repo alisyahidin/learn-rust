c3f4f08fa7989d8dbcdd65f1bdf075c3d5b2388d
Ali Syahidin <ali.syahidin@sg-dsa.com>
2021-10-19 13:26:14 +0700

    add understanding ownership

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+fn fib(num: i32) -> i32 {
+    match num {
+        0 => 0,
+        1 => 1,
+        2 => 1,
+        _ => fib(num - 1) + fib(num - 2),
+    }
+}
+
 fn get_loop() {
     let mut y: u32 = 1;
     let max_loop: u32 = 10;
@@ -34,6 +43,32 @@ fn multiply_by10(x: u32) -> u32 {
     (y + x) * 10
 }
 
+fn understand_ownership() {
+    let s1 = String::from("Test"); // string store in heap, NOT in STACK
+    let s2 = s1.clone(); // clone the value, because value has stored in HEAP cannot have multiple owner a.k.a variable
+    println!("string in heap {} {}", s1, s2);
+
+    let x = "test";
+    let _y = x; // it's coppied because data stored in STACK
+    println!("{}", x);
+
+    let s = String::from("test");
+    take_ownership(s);
+    // s variable is no longer valid here
+
+    let t: u32 = 10;
+    copy_value(t);
+    // println!("{}", t); // t is still valid because function only copy the value from params
+}
+
+fn take_ownership(a: String) {
+    println!("{}", a); // value in parameter a will be dropped
+}
+
+fn copy_value(a: u32) {
+    println!("{}", a);
+}
+
 fn main() {
     println!("Hello, rust!");
 
@@ -44,9 +79,13 @@ fn main() {
 
     println!("test : {{ {} }}", x);
 
-    println!("getNumber {}", multiply_by10(10));
+    println!("fibonaci 40: {}", fib(40));
 
     get_loop();
+
+    understand_ownership();
+
+    println!("getNumber {}", multiply_by10(10));
 
     let secret_number = rand::thread_rng().gen_range(1..10);
 
