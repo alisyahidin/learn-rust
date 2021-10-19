6fe4aab3703c5b0c109bc64fcc424d722f27203f
Ali Syahidin <ali.syahidin@sg-dsa.com>
2021-10-19 11:08:10 +0700

    add loop function

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,51 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+fn get_loop() {
+    let mut y: u32 = 1;
+    let max_loop: u32 = 10;
+    loop {
+        let mut x: u32 = 1;
+        loop {
+            if x == max_loop {
+                break;
+            }
+            if x == y || max_loop - y == x {
+                print!("*");
+            } else {
+                print!(" ");
+            }
+            x += 1;
+        }
+        println!("");
+        if y == max_loop {
+            break;
+        };
+        y += 1;
+    }
+}
+
+fn multiply_by10(x: u32) -> u32 {
+    let y: u32 = {
+        let x = 50;
+        x + 1
+    };
+    (y + x) * 10
+}
+
 fn main() {
     println!("Hello, rust!");
+
+    let mut x: u8 = 5;
+    println!("test : {{ {} }}", x);
+
+    x = 4;
+
+    println!("test : {{ {} }}", x);
+
+    println!("getNumber {}", multiply_by10(10));
+
+    get_loop();
 
     let secret_number = rand::thread_rng().gen_range(1..10);
 
